06c27bcc58b32f17258f22844cfc6aab0ec7c450
Daniel Jordi Perez <danipq26@gmail.com>
2017-11-28 15:26:51 +0100

    green: trip service returns no trips if the user hasn't friends

--- src/test/java/org/craftedsw/tripservicekata/trip/TripServiceShould.java
+++ src/test/java/org/craftedsw/tripservicekata/trip/TripServiceShould.java
@@ -1,16 +1,32 @@
 package org.craftedsw.tripservicekata.trip;
 
 import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
+import org.craftedsw.tripservicekata.user.User;
 import org.junit.Test;
+
+import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class TripServiceShould {
 
+    private User loggedInUser = new User();
+
     @Test(expected = UserNotLoggedInException.class)
     public void not_return_trips_if_the_user_is_not_logged_in() {
         TripService tripService = new TestableTripService(null);
         tripService.getTripsByUser(null);
+    }
+
+    @Test
+    public void not_return_trips_is_user_has_no_friends() {
+        TripService tripService = new TestableTripService(loggedInUser);
+        tripService.getTripsByUser(loggedInUser);
+        User billyNoMates = new User();
+
+        List<Trip> trips = tripService.getTripsByUser(billyNoMates);
+
+        assertEquals(0, trips.size());
     }
 
 }
