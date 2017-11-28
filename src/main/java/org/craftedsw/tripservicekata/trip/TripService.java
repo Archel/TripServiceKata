7fef05b46bda2ce362d5592882642a862cbbf086
Daniel Jordi Perez <danipq26@gmail.com>
2017-11-28 16:57:01 +0100

    Refactor get logged user

--- src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
+++ src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
@@ -2,6 +2,7 @@ package org.craftedsw.tripservicekata.trip;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
@@ -10,23 +11,26 @@ import org.craftedsw.tripservicekata.user.UserSession;
 public class TripService {
 
 	public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
-		List<Trip> tripList = new ArrayList<Trip>();
-		User loggedUser = getLoggedUser();
+		List<Trip> tripList = new ArrayList<>();
+
+		User loggedUser = Optional
+                            .ofNullable(getLoggedUser())
+                            .orElseThrow(UserNotLoggedInException::new);
+
 		boolean isFriend = false;
-		if (loggedUser != null) {
-			for (User friend : user.getFriends()) {
-				if (friend.equals(loggedUser)) {
-					isFriend = true;
-					break;
-				}
-			}
-			if (isFriend) {
-				tripList = findTripsByUser(user);
-			}
-			return tripList;
-		} else {
-			throw new UserNotLoggedInException();
-		}
+
+		for (User friend : user.getFriends()) {
+            if (friend.equals(loggedUser)) {
+                isFriend = true;
+                break;
+            }
+        }
+
+        if (isFriend) {
+            tripList = findTripsByUser(user);
+        }
+        
+        return tripList;
 	}
 
 	protected List<Trip> findTripsByUser(User user) {
